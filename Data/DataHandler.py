48de959971c43641459a4cec39172c346ba7688e
byrce <bryce.r.harrington@gmail.com>
2022-03-11 08:46:46 -0500

    Add main files / folders Add TODO.txt describing overview

---
+++ Data/DataHandler.py
@@ -0,0 +1,22 @@
+"""
+DataHandler
+
+Takes in our raw data / labels into an ML friendly format. Handles pulling statistics from data as well
+
+Bryce Harrington
+03/11/2022
+"""
+import numpy as np
+
+
+class DataHandler:
+    def __init__(self, target_size: tuple = (300, 300)):
+        pass
+
+    def print(self):
+        print("asdfaS")
+
+
+if __name__ == "__main__":
+    dh = DataHandler()
+    dh.print()
---
+++ TrafficLightDetector.py
@@ -0,0 +1,14 @@
+"""
+TrafficLightDetector
+
+Inferences data through our traffic light detection model, returns observations for light status / location in image
+
+Bryce Harrington
+03/11/2022
+"""
+import tensorflow as tf
+
+
+class TrafficLightDetector:
+    def __init__(self, model_name: str):
+        pass
